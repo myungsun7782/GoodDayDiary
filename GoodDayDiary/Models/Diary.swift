f29fb06df3bbcae78fc5e628587501a70baf29e6
myungsun <audtjs9987@naver.com>
2022-10-30 21:45:59 +0900

    feat: make Diary model

---
+++ GoodDayDiary/Models/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  GoodDayDiary
+//
+//  Created by myungsun on 2022/10/30.
+//
+
+import UIKit
+
+struct Diary {
+    var title: String!
+    var contents: String!
+    var date: String!
+    var photoList: [UIImage]?
+}
--- GoodDayDiary/SceneDelegate.swift
+++ GoodDayDiary/SceneDelegate.swift
@@ -18,6 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
+        // MARK: - 앱 처음 진입 시 UUID 생성
         if !UserDefaultsManager.shared.getIsInitialized() {
             let uuid: String = UUID().uuidString
             
